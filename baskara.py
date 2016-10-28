2dc525a80f83838639bba86dd614341fce604ed6
José Luis da Cruz Junior <zejuniortdr@gmail.com>
2016-10-28 15:24:58 -0200

    baskara

--- baskara.py
+++ baskara.py
@@ -8,9 +8,26 @@ def log(txt):
 	print "===================================="
 
 
-def baskara():
+def baskara(a,b,c):
 	from math import sqrt
 
+
+	DELTA = pow(b,2) - 4*a*c
+
+	if DELTA == 0:
+		x1 = x2 = -b / 2*a
+		return [x1,x2]
+
+	elif DELTA > 0:
+		x1 = (-b + sqrt(DELTA)) / 2*a
+		x2 = (-b - sqrt(DELTA)) / 2*a
+		return [x1,x2]
+	else:
+		return None
+
+
+
+if __name__ == '__main__':
 	log("Baskara Formula")
 	log("axˆ2 + bx + c = 0")
 
@@ -18,23 +35,10 @@ def baskara():
 	b = int(input("Type value of B:"))
 	c = int(input("Type value of C:"))
 
-	DELTA = pow(b,2) - 4*a*c
+	solutions = baskara(a,b,c)
 
-	if DELTA == 0:
-		x1 = x2 = (-b + sqrt(DELTA)) / 2*a
-		log("Solutions: {0}, {1}".format(x1,x2))
-
-	elif DELTA >= 0:
-		x1 = (-b + sqrt(DELTA)) / 2*a
-		x2 = (-b - sqrt(DELTA)) / 2*a
-		log("Solutions: {0}, {1}".format(x1,x2))
-
+	if solutions:
+		log("Solutions: {0}".format(solutions))
 	else:
 		log("{0}xˆ2 + {1}x + {2} = 0 | Doesn't have Real solutions".format(a,b,c))
-
-
-
-if __name__ == '__main__':
-	baskara()
-
 
