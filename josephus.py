8a13ae353c21993b00d9bf711ac92f662ca2a973
José Luis da Cruz Junior <zejuniortdr@gmail.com>
2016-10-28 14:02:50 -0200

    josephus

--- josephus.py
+++ josephus.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-def log(texto):
+def log(txt):
 	print "===================================="
-	print texto
+	print txt
 	print "===================================="
 
 
 def josephus():
-	participantes = int(input("Number of participants: "))
-	binario = list(str(bin(participantes)).replace('0b', ''))
+	participants = int(input("Number of participants: "))
+	binary_form = list(str(bin(participants)).replace('0b', ''))
 	i = 0
-	for a in binario:
+	for a in binary_form:
 		if a == '1':
-			binario[i] = '0'
-			binario.append('1')
+			binary_form[i] = '0'
+			binary_form.append('1')
 			break
 		i += 1
 
-	log("Wining Seat: {0}".format(int('0b{0}'.format("".join(binario)),2)))
+	log("Wining Seat: {0}".format(int('0b{0}'.format("".join(binary_form)),2)))
 
 if __name__ == '__main__':
 	josephus()
